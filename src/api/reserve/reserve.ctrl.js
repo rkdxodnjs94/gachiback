8cc98cb7fa4be850f827743e394de737fb498a05
gangtaewon <rkdxodnjs94@naver.com>
2022-06-06 16:10:33 +0900

    6/6 예약기능 구축완료

--- src/api/reserve/reserve.ctrl.js
+++ src/api/reserve/reserve.ctrl.js
@@ -70,7 +70,8 @@ export const list = async (context) => {
 /** GET /api/reserve/place **/
 export const placeread = async (context) => {
   try {
-    const reserve = await Reserve.find({place : context.query.place}).select('place arrage').exec();
+    const reserve = await Reserve.find({place : context.query.place})
+    .select('place arrage').exec();
     if (!reserve) {
       context.status = 404; // Not Found
       return;
